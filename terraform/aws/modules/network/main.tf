bb8811a328b0dbe1264e749abfab2f95c4fd2dc9
wesship <37167955+wesship@users.noreply.github.com>
2026-05-24 09:21:40 -0600

    fix(terraform): declare network module inputs

--- terraform/aws/modules/network/main.tf
+++ terraform/aws/modules/network/main.tf
@@ -1,3 +1,25 @@
+variable "name_prefix" {
+  description = "Name prefix used for network resources."
+  type        = string
+}
+
+variable "cidr_block" {
+  description = "CIDR block for the VPC."
+  type        = string
+}
+
+variable "az_count" {
+  description = "Number of availability zones to prepare for."
+  type        = number
+  default     = 2
+}
+
+variable "tags" {
+  description = "Tags applied to network resources."
+  type        = map(string)
+  default     = {}
+}
+
 resource "aws_vpc" "this" {
   cidr_block           = var.cidr_block
   enable_dns_support   = true
@@ -18,4 +40,8 @@ output "private_subnet_ids" {
 
 output "public_subnet_ids" {
   value = []
+}
+
+output "az_count" {
+  value = var.az_count
 }
