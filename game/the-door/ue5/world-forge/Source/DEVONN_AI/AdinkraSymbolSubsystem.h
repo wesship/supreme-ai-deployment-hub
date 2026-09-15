848ae6c55b37b50145799f8cbebc366abe7716a9
wesship <37167955+wesship@users.noreply.github.com>
2026-09-15 11:02:04 -0600

    feat(game): add Adinkra symbol subsystem

---
+++ game/the-door/ue5/world-forge/Source/DEVONN_AI/AdinkraSymbolSubsystem.h
@@ -0,0 +1,36 @@
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Subsystems/GameInstanceSubsystem.h"
+#include "AdinkraSymbolTypes.h"
+#include "AdinkraSymbolSubsystem.generated.h"
+
+UCLASS()
+class DEVONN_AI_API UAdinkraSymbolSubsystem : public UGameInstanceSubsystem
+{
+    GENERATED_BODY()
+
+public:
+    virtual void Initialize(FSubsystemCollectionBase& Collection) override;
+
+    UFUNCTION(BlueprintCallable, Category="The Door|Adinkra")
+    bool ResolveSymbol(FName SymbolID, FAdinkraSymbolDefinition& OutDefinition) const;
+
+    UFUNCTION(BlueprintCallable, Category="The Door|Adinkra")
+    TArray<FAdinkraSymbolDefinition> GetSymbols() const;
+
+    UFUNCTION(BlueprintCallable, Category="The Door|Adinkra")
+    bool DiscoverSymbol(FName SymbolID, FName WorldID);
+
+    UFUNCTION(BlueprintCallable, Category="The Door|Adinkra")
+    bool GetDiscoveryState(FName SymbolID, FAdinkraDiscoveryState& OutState) const;
+
+    UFUNCTION(BlueprintCallable, Category="The Door|Adinkra")
+    bool IsSymbolVisibleAtGnosis(FName SymbolID, int32 Gnosis) const;
+
+private:
+    void RegisterSeedSymbols();
+
+    TMap<FName, FAdinkraSymbolDefinition> Symbols;
+    TMap<FName, FAdinkraDiscoveryState> Discoveries;
+};
